85f97d51ce21c6f9a507372af1d13b08b5ee860a
StefanPDavis <snoodle22@hotmail.com>
2024-02-06 17:02:46 -0800

    bug fix

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,25 +2,14 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#blog-name').value.trim();
-    const needed_funding = document.querySelector('#blog-funding').value.trim();
     const description = document.querySelector('#blog-desc').value.trim();
-  
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/blogs`, {
-        method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
   
       if (response.ok) {
         document.location.replace('/profile');
       } else {
         alert('Failed to create blog');
       }
-    }
-  };
+    };
   
   const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
